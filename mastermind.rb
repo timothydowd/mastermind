8dbcea15521cc1a7c54a5e4037ff22380f716080
dwdy250 <timdowd1984@gmail.com>
2018-08-21 12:48:56 +0100

    started structure

---
+++ mastermind.rb
@@ -0,0 +1,41 @@
+
+
+class Game
+
+  def initialize
+
+  end
+end
+
+class Board
+
+  def board_array
+    @board_array
+  end
+
+  def board_array=(board_array)
+    @board_array = board_array
+  end
+  
+  def initialize
+    @board_array = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+  end
+
+
+end
+
+class Player
+  def initialize
+  end
+
+end
+
+class Computer
+end
+
+class Letter
+
+end
+
+class ClueGiver
+end
